477c122c433c0cb30bb0f4ebbff666e96f24b190
Nancy J Campbell <124710465+NJCampbell@users.noreply.github.com>
2023-09-15 18:38:08 +0000

    private only accessible with valid token

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -5,15 +5,17 @@ import { Context } from "../store/appContext";
 const Private = () => {
     const { store, actions } = useContext(Context);
 
-    {
+    if (store.token || store.token === "" || store.token === undefined) {
+        {
 
-        return (
+            return (
 
-            <div className="text-center mt-5">
-                <h1>Welcome to the exclusive private page!</h1>
-                <h2>Only authorized users can see this area!</h2>
-                <h3>Lucky you!</h3>
-            </div>)
+                <div className="text-center mt-5">
+                    <h1>Welcome to the exclusive private page!</h1>
+                    <h2>Only authorized users can see this area!</h2>
+                    <h3>Lucky you!</h3>
+                </div>)
+        }
     }
 };
 
