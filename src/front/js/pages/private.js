3df25c2eb5228b9ffdbcbdb1ad88e1bd407f8f92
Nancy J Campbell <124710465+NJCampbell@users.noreply.github.com>
2023-09-15 15:51:26 +0000

    private added

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,13 +1,29 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
+import { Navbar } from "../component/navbar";
 
-export const Private = () => {
+const Private = () => {
     const { store, actions } = useContext(Context);
 
     return (
-        <>
-        <h1>Private Area, Only Authorized Users Allowed!</h1>
-        </>
-    )
-}
+        // (store.token ? <div className="text-center mt-5">
+
+        //     {/* <button onClick={handleLogout} className="btn btn-secondary">
+        // 	Logout
+        // </button> */}
+        //     <h1>{store.homeMessage}</h1>
+        //     <Navbar />
+
+        // </div> : 
+        <div className="text-center mt-5">
+            <h1>Welcome to the exclusive private page!</h1>
+            <h2>Only authorized users can see this area!</h2>
+            <h3>Lucky you!</h3>
+
+
+        </div>)
+
+};
+
+export default Private;
